c3b86ec84e185e60941f8526c1413107235b3d06
Kosolapov Roman <50093493+KosolapovR@users.noreply.github.com>
2020-08-10 14:28:42 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const port = "8080";
+
+const jsonParser = bodyParser.json();
+
+const fixtures = {
+  users: [
+    { id: 1, name: "user1" },
+    { id: 2, name: "user2" },
+    { id: 3, name: "user3" }
+  ]
+};
+
+app.use(jsonParser);
+
+app.get("/users", (req, res) => {
+  res.send(fixtures.users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const user = fixtures.users.find((u) => u.id === parseInt(req.params.id, 10));
+
+  res.send(user);
+});
+
+app.post("/users", (req, res) => {
+  if (req.body.name) {
+    const newUserId = fixtures.user[fixtures.users.length - 1].id + 1;
+    const user = { id: newUserId, name: req.body.name };
+
+    fixtures.users.push(user);
+
+    res.send(fixtures.user[fixtures.users.length - 1]);
+  }
+});
+
+app.delete("/users/:id", (req, res) => {
+  let deleted = false;
+  fixtures.user = fixtures.user.filter((u) => {
+    if (u.id !== req.params.id) {
+      return u;
+    } else {
+      deleted = true;
+      return u;
+    }
+  });
+
+  res.send(deleted);
+});
+
+app.listen(port, () => {
+  console.log("server started on port: ", port);
+});
